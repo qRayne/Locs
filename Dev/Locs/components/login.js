f9fb24b01082e0e09fb9c8a61529dc126d37f92d
Lemar Andar <lemarandar@pm.me>
2023-03-09 11:33:11 -0500

    Login page started

--- Dev/Locs/components/login.js
+++ Dev/Locs/components/login.js
@@ -1,19 +1,38 @@
-import { Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Pressable, Text, TextInput, View } from 'react-native';
 import loginStyles from '../styles/loginStyles';
 
 export default function Login() {
-  return (
-    <View style={loginStyles.container}>
-      <View>
-        <Text style={loginStyles.title}>Locs</Text>
-      </View>
-      <TextInput 
-        style={loginStyles.inputbox}
-        placeholder="Username ou email"
-       />
-      <TextInput style={loginStyles.inputbox}
-        placeholder="Mot de passe"
-      />
-    </View>
-  );
+    const [email, setEmail] = useState("");
+    const [pwd, setPwd] = useState("");
+
+    return (
+        <View style={loginStyles.container}>
+          <View>
+            <Text style={loginStyles.title}>Locs</Text>
+          </View>
+          <TextInput 
+            style={loginStyles.inputbox}
+            placeholder="Username ou email"
+            value={email}
+            onChangeText={setEmail}
+           />
+          <TextInput 
+            style={loginStyles.inputbox}
+            placeholder="Mot de passe"
+            value={pwd}
+            onChangeText={setPwd}
+          />
+          <Pressable 
+            style={loginStyles.button}
+            onPressIn={() => {console.log(email, pwd)}}>
+                <Text style={loginStyles.text}>Login</Text>
+          </Pressable>
+
+          <Pressable
+            onPressIn={() => {console.log("move to register screen")}}>
+            <Text style={loginStyles.register}> Register? </Text>
+          </Pressable>
+        </View>
+    );
 }
--- Dev/Locs/styles/loginStyles.js
+++ Dev/Locs/styles/loginStyles.js
@@ -2,7 +2,6 @@ import { StyleSheet } from "react-native";
 
 const loginStyles = StyleSheet.create({
     container: {
-      fontFamily: "Galdeano-Regular",
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
@@ -10,17 +9,38 @@ const loginStyles = StyleSheet.create({
     },
   
     title:{
-      fontSize: 50
+      fontSize: 50,
+      marginBottom: 25
     },
   
     inputbox: {
       backgroundColor: '#3C746F',
       alignItems: 'center',
       justifyContent: 'center',
+      borderRadius: 10,
       width: 250,
       margin: 7,
       padding: 5,
       color: 'white'
+    },
+
+    button:{
+      backgroundColor: "#000",
+      width:75,
+      height: 30,
+      flex:-1,
+      marginTop: 15,
+      justifyContent: 'center',
+      alignItems: 'center', 
+      borderRadius: 10,
+    },
+
+    text:{
+      color: 'white'
+    },
+
+    register:{
+      marginTop: 15
     }
   });
 
