6a3f39eb183e17e451a3eef08d87b85edacde199
qRayne <kennafrayanerachid@gmail.com>
2023-03-15 17:32:55 -0400

    login works

--- Dev/Locs/components/login.js
+++ Dev/Locs/components/login.js
@@ -1,48 +1,72 @@
 import { useState } from 'react';
-import { Button, Pressable, Text, TextInput, View } from 'react-native';
+import { Button, Pressable, Text, TextInput, View } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import globalStyles from '../styles/globalStyles';
 import loginStyles from '../styles/loginStyles';
 
-export default function Login({navigation}) {
-    const [email, setEmail] = useState("");
-    const [pwd, setPwd] = useState("");
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
 
-    return (
-        <View style={globalStyles.container}>
-          <View>
-            <Text style={loginStyles.title}>Locs</Text>
-          </View>
-          <TextInput 
-            style={globalStyles.inputbox}
-            placeholder="Username ou email"
-            value={email}
-            autoComplete="username"
-            onChangeText={setEmail}
-           />
-          <TextInput 
-            style={globalStyles.inputbox}
-            placeholder="Mot de passe"
-            autoComplete="password"
-            secureTextEntry={true}
-            value={pwd}
-            onChangeText={setPwd}
-          />
-          <Pressable 
-            style={globalStyles.button}
-            onPressIn={() => {
-              console.log(email, pwd);
-              navigation.navigate('ChatAutour');
-            }}>
-            <Text style={globalStyles.text}>Login</Text>
-          </Pressable>
+  async function login() {
+    try {
+      const response = await fetch("http://192.168.2.20:3000/login-User", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: pwd
+        })
+      });
+      const responseData = await response.json();
+      if (response.ok) {
+        // la connection marche et on recupere le token du fetch
+        // le token prouve que l'usager s'est connecter à notre application
+        await AsyncStorage.setItem('token', responseData.token);
+        navigation.navigate('ChatAutour');
+      } else {
+        console.log(responseData.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-          <Pressable
-            onPressIn={() => {
-              console.log("move to register screen");
-              navigation.navigate('Register');
-            }}>
-            <Text style={globalStyles.register}> Register? </Text>
-          </Pressable>
-        </View>
-    );
+  return (
+    <View style={globalStyles.container}>
+      <View>
+        <Text style={loginStyles.title}>Locs</Text>
+      </View>
+      <TextInput
+        style={globalStyles.inputbox}
+        placeholder="Username ou email"
+        value={email}
+        autoComplete="username"
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={globalStyles.inputbox}
+        placeholder="Mot de passe"
+        autoComplete="password"
+        secureTextEntry={true}
+        value={pwd}
+        onChangeText={setPwd}
+      />
+      <Pressable
+        style={globalStyles.button}
+        onPressIn={login}>
+        <Text style={globalStyles.text}>Login</Text>
+      </Pressable>
+
+      <Pressable
+        onPressIn={() => {
+          console.log("move to register screen");
+          navigation.navigate('Register');
+        }}>
+        <Text style={globalStyles.register}> Register? </Text>
+      </Pressable>
+    </View>
+  );
 }
