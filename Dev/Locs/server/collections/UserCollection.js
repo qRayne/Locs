20e5f71c6898784404e813d8709ee24501be4099
qRayne <kennafrayanerachid@gmail.com>
2023-03-25 14:44:11 -0400

    added restrinction to password input (one upper letter, one lower letter, one special symbol and a minimal lenght of 8

--- Dev/Locs/server/collections/UserCollection.js
+++ Dev/Locs/server/collections/UserCollection.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 // on créer un schema avec toutes nos champs et leurs types
 const UserSchema = new mongoose.Schema({
     email: { type: String, unique: true, match: /.+\@.+\..+/, required: true },
-    password: { type: String, required: true },
+    password: { type: String,match: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/,required: true},
     profile: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }
 });
 
