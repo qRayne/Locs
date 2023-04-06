5325caaa6340f99be47ac8e0dada0dadd1a1ce7a
Lemar Andar <lemarandar@pm.me>
2023-04-06 09:21:05 -0400

    we are now sure that currentlocation work

--- Dev/Locs/components/location.js
+++ Dev/Locs/components/location.js
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import {Pressable, Text, TextInput, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Pressable, Text, TextInput, View } from 'react-native';
 import globalStyles from '../styles/globalStyles';
 import locationStyles from '../styles/locationStyles';
 import * as Loc from 'expo-location';
@@ -17,38 +17,38 @@ export default function Location({navigation}) {
     console.log(location.coords.latitude, location.coords.longitude);
   }
 
-  getCurrentLocation();
-  
-    return (
-        <View style={globalStyles.container}>
-          <View>
-            <Text style={globalStyles.subtitle}>Location</Text>
-          </View>
+  setTimeout(getCurrentLocation,5000);
 
-          <TextInput>
+  return (
+    <View style={globalStyles.container}>
+      <View>
+        <Text style={globalStyles.subtitle}>Location</Text>
+      </View>
 
-          </TextInput>
-          <MapView
-            provider={PROVIDER_GOOGLE}
-            style={locationStyles.map}
-            region={{
-              latitude: 42.882004,
-              longitude: 74.582748,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421
-            }}        
-            showsUserLocation={true}
-            followsUserLocation={true}
-          />
-          {/* Sprint 2 - A commencer */}
+      <TextInput>
 
-          <Pressable
-            onPressIn={() => {
-              console.log("move to chatAutour screen");
-              navigation.navigate('ChatAutour')
-            }}>
-            <Text style={globalStyles.register}> Leave? </Text>
-          </Pressable>
-        </View>
-    );
+      </TextInput>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={locationStyles.map}
+        region={{
+          latitude: 45.5147,
+          longitude: -73.5664,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}        
+        showsUserLocation={true}
+        followsUserLocation={true}
+      />
+      {/* Sprint 2 - A commencer */}
+
+      <Pressable
+        onPressIn={() => {
+          console.log("move to chatAutour screen");
+          navigation.navigate('ChatAutour')
+        }}>
+        <Text style={globalStyles.register}> Leave? </Text>
+      </Pressable>
+    </View>
+  );
 }
